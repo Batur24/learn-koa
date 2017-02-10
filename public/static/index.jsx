f9bcdd819e10dee92e77f9303e959d46300da768
Batur <heshupai@gmail.com>
2017-02-10 20:48:40 +0800

    use webpack to bundle react bootstrap jsx file

---
+++ public/static/index.jsx
@@ -0,0 +1,17 @@
+/**
+ * Created by batur on 2017/2/10.
+ */
+
+//var Alert = require('react-bootstrap').Alert;
+import React from 'react';
+import { render } from 'react-dom';
+import { Button } from 'react-bootstrap';
+
+
+const Btn = (
+  <Button>Default</Button>
+);
+
+render(Btn, document.getElementById('sBtn'));
+
+
