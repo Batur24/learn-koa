5fe63e9e927d2563e81d8f8a150cd9d620a77545
Batur <heshupai@gmail.com>
2017-02-12 23:45:59 +0800

    use webpack to bundle front end code

--- public/static/index.jsx
+++ public/static/index.jsx
@@ -5,13 +5,22 @@
 //var Alert = require('react-bootstrap').Alert;
 import React from 'react';
 import { render } from 'react-dom';
-import { Button } from 'react-bootstrap';
+import { Button, FormGroup, FormControl } from 'react-bootstrap';
 
 
 const Btn = (
-  <Button>Default</Button>
+  <Button bsStyle="primary">Success</Button>
 );
 
+const inputInstance = (
+  <FormGroup bsSize="large">
+    <FormControl type="text" />
+  </FormGroup>
+);
+
+![Gitter](https://badges.gitter.im/gitterHQ/gitter.svg)](https://gitter.im/nodejsWeb/Lobby)
+
 render(Btn, document.getElementById('sBtn'));
+render(inputInstance, document.getElementById('search'));
 
 
